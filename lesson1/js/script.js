17b7e5daf326647517f1fa1a856878a104d17b29
Nikita <pishkin1112@gmail.com>
2019-04-22 12:26:48 +0700

    #4 Commit. Studying cycles & conditions. Added input check.

--- lesson1/js/script.js
+++ lesson1/js/script.js
@@ -1,35 +1,83 @@
 'use strict';
 
-let money = +prompt("Ваш бюджет на месяц?", "111111"),
-    time = prompt("Введите дату в формате YYYY-MM-DD", "YYYY-MM-DD");
+let money = +prompt("Ваш бюджет на месяц?", "800000"),
+  time = prompt("Введите дату в формате YYYY-MM-DD", "YYYY-MM-DD");
 
-// let money = 111111,
-//   time = "YYYY-MM-DD";
 
 let appData = {
-    moneyBudget: money, //бюджет из переменной
-    timeDate: time, //дата из переменной
-    expenses: {}, //обязательные расходы
-    optionalExpenses: {}, //необязательные расходы(пусто)
-    extraIncome: {}, //доп доход
-    savings: false //накопления
+  moneyBudget: money, //бюджет из переменной
+  timeDate: time, //дата из переменной
+  expenses: {}, //обязательные расходы
+  optionalExpenses: {}, //необязательные расходы(пусто)
+  extraIncome: {}, //доп доход
+  savings: false //накопления
 };
 
-let mandatoryExpense = prompt("Введите обязательную статью расходов в этом месяце", "Еда"),
-    costMandExp = prompt("Во сколько обойдется?", "5");
-    
-appData.expenses[mandatoryExpense] = costMandExp;
+
+for (let i = 0; i < 2; i++) {
+  let mandExp = prompt("Введите обязательную статью расходов в этом месяце", ""),
+    //mandExp - mandatoryExpenses - обязательные расходы
+    costMandExp = prompt("Во сколько обойдется?", "");
+
+  if ((typeof (mandExp)) === 'string' && (typeof (mandExp)) != null && (typeof (costMandExp)) != null &&
+    mandExp != '' && costMandExp != '' && mandExp.length < 50) {
+    console.log("Allright");
+    appData.expenses[mandExp] = costMandExp;
+  } else {
+    alert("Введите пожалуйста реальные данные статьи расходов и затраты на нее. Название статьи расходов не должно превышать 50 символов.");
+    --i; //не верится, что так просто...
+  }
+};
 
 
-mandatoryExpense = prompt("Введите обязательную статью расходов в этом месяце", "Транспорт");
-costMandExp = prompt("Во сколько обойдется?", "7");
+//ЦИКЛ WHILE
+// let i = 0;
+// while (i < 2) {
+//   let mandExp = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//     //mandExp - mandatoryExpenses - обязательные расходы
+//     costMandExp = prompt("Во сколько обойдется?", "");
+//     if ((typeof (mandExp)) === 'string' && (typeof (mandExp)) != null && (typeof (costMandExp)) != null &&
+//       mandExp != '' && costMandExp != '' && mandExp.length < 50) {
+//       console.log("Allright");
+//       appData.expenses[mandExp] = costMandExp;
+//     } else {
+//       alert("Введите пожалуйста реальные данные статьи расходов и затраты на нее. Название статьи расходов не должно превышать 50 символов.");
+//       --i; //не верится, что так просто...
+//     }
+//     i++;
+// }
 
-appData.expenses[mandatoryExpense] = costMandExp;
+
+//ЦИКЛ DO..WHILE
+// let i = 0;
+// do {
+//   let mandExp = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//     //mandExp - mandatoryExpenses - обязательные расходы
+//     costMandExp = prompt("Во сколько обойдется?", "");
+//   if ((typeof (mandExp)) === 'string' && (typeof (mandExp)) != null && (typeof (costMandExp)) != null &&
+//     mandExp != '' && costMandExp != '' && mandExp.length < 50) {
+//     console.log("Allright");
+//     appData.expenses[mandExp] = costMandExp;
+//   } else {
+//     alert("Введите пожалуйста реальные данные статьи расходов и затраты на нее. Название статьи расходов не должно превышать 50 символов.");
+//     --i; //не верится, что так просто...
+//   }
+//   i++;
+// } while (i < 2);
+
+
+appData.moneyPerDay = appData.moneyBudget / 30
+
+alert("Бюджет на 1 день: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+  console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+  console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+  console.log("Высокий уровень достатка..");
+} else {
+  console.log("Ошибка");
+}
 
 console.log(appData);
-
-alert("Бюджет на 1 день " + appData.moneyBudget / 30);
-
-// console.log(typeof(appData.moneyBudget));
-
-
