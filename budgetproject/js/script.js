48523fd1a532f0751ab12bc8858fc4079f0a05c4
Nikita <pishkin1112@gmail.com>
2019-04-24 02:55:08 +0700

    #7 Commit. Modifying project throught OOP. Some objects & array stuff.

--- budgetproject/js/script.js
+++ budgetproject/js/script.js
@@ -6,9 +6,9 @@ function start() {
   money = +prompt("Ваш бюджет на месяц?", "800000");
   time = prompt("Введите дату в формате YYYY-MM-DD", "YYYY-MM-DD");
 
-  while(isNaN(money) || money  == "" || money == null) { //проверяем на "не цифры" или "пустую строку"
-  // или "нажатие кнопки отмена"
-  money = +prompt("Ваш бюджет на месяц?", "800000");
+  while (isNaN(money) || money == "" || money == null) { //проверяем на "не цифры" или "пустую строку"
+    // или "нажатие кнопки отмена"
+    money = +prompt("Ваш бюджет на месяц?", "800000");
   }
 }
 start();
@@ -19,75 +19,92 @@ let appData = {
   timeDate: time, //дата из переменной
   expenses: {}, //обязательные расходы
   optionalExpenses: {}, //необязательные расходы(пусто)
-  extraIncome: {}, //доп доход
-  savings: true //накопления
-};
-
-function chooseExpenses() { //обернули 2 ввода с проверками в функции
-  for (let i = 0; i < 2; i++) {
-    let mandExp = prompt("Введите обязательную статью расходов в этом месяце", "x"),
-      //mandExp - mandatoryExpenses - обязательные расходы
-      costMandExp = prompt("Во сколько обойдется?", "1");
+  extraIncome: [], //доп доход
+  savings: true, //накопления
+  chooseExpenses: function () {
+    for (let i = 0; i < 2; i++) {
+      let mandExp = prompt("Введите обязательную статью расходов в этом месяце", "x"),
+        //mandExp - mandatoryExpenses - обязательные расходы
+        costMandExp = prompt("Во сколько обойдется?", "1");
       console.log(typeof (costMandExp));
       console.log(typeof (mandExp));
 
-
-
-    if ((typeof (mandExp)) === 'string' && (typeof (mandExp)) !== null && (typeof (costMandExp)) != null &&
-      mandExp != '' && costMandExp != '' && mandExp.length < 50) {
-      console.log("Allright");
-      appData.expenses[mandExp] = costMandExp;
-    } else {
-      alert("Введите пожалуйста реальные данные статьи расходов и затраты на нее. Название статьи расходов не должно превышать 50 символов.");
-      --i; //не верится, что так просто...
-    }
-  }
-}
-chooseExpenses();
-
-
-function detectDayBudget() {
-  appData.moneyPerDay = (appData.moneyBudget / 30).toFixed(2);
-  //этот метод округляет число, причем именно число
-  alert("Бюджет на 1 день: " + appData.moneyPerDay);
-}
-detectDayBudget();
-
-function detectLevel() {
-  if (appData.moneyPerDay < 100) {
-    console.log("Минимальный уровень достатка");
-  } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log("Средний уровень достатка");
-  } else if (appData.moneyPerDay > 2000) {
-    console.log("Высокий уровень достатка..");
-  } else {
-    console.log("Ошибка");
-  }
-}
-detectLevel();
-
-function checkSavings() {
-  if (appData.savings == true) {
-    let save = +prompt("Какова сумма накоплений?","31530000"),
-        percent = +prompt("Под какой процент?","6");
-
-    appData.monthIncome = save/100/12*percent;
-    alert("Доход от вашего депозита в месяц: " + appData.monthIncome);
-  }
-}
-checkSavings();
-
-
-function chooseOptExpenses() {
-  for (let i = 0; i < 3; i++) {
-    let optExp = prompt("Статья необязательных расходов?", "Games");
-      while ((typeof (optExp)) !== 'string' || optExp == "" || optExp == null) {
+      if ((typeof (mandExp)) === 'string' && mandExp != null && costMandExp != null &&
+        mandExp != '' && costMandExp != '' && mandExp.length < 50) {
+        console.log("Allright");
+        appData.expenses[mandExp] = costMandExp;
+      } else {
+        // alert("Введите пожалуйста реальные данные статьи расходов и затраты на нее. Название статьи расходов не должно превышать 50 символов.");
         --i;
       }
-      console.log("ok");      
-      appData.optionalExpenses[i + 1] = optExp;        
-  }
-}
-chooseOptExpenses();
+    }
+  },
+  detectDayBudget: function () {
+    appData.moneyPerDay = (appData.moneyBudget / 30).toFixed(2);
+    //этот метод округляет число, причем именно число
+    alert("Бюджет на 1 день: " + appData.moneyPerDay);
+  },
+  detectLevel: function () {
+    if (appData.moneyPerDay < 100) {
+      console.log("Минимальный уровень достатка");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+      console.log("Средний уровень достатка");
+    } else if (appData.moneyPerDay > 2000) {
+      console.log("Высокий уровень достатка..");
+    } else {
+      console.log("Ошибка");
+    }
+  },
+  checkSavings: function () {
+    if (appData.savings == true) {
+      let save = +prompt("Какова сумма накоплений?", "31530000"),
+        percent = +prompt("Под какой процент?", "6");
 
-console.log(appData);
+      appData.monthIncome = save / 100 / 12 * percent;
+      alert("Доход от вашего депозита в месяц: " + appData.monthIncome);
+    }
+  },
+  chooseOptExpenses: function () {
+    for (let i = 0; i < 3; i++) {
+      let optExp = prompt("Статья необязательных расходов?", "Games");
+      if ((typeof (optExp)) === 'string' && optExp != null && optExp != '') {
+        console.log("ok");
+        appData.optionalExpenses[i + 1] = optExp;
+      } else {
+        --i;
+      }
+    }
+  },
+  chooseIncome: function() {
+
+    for (let i = 0; i < 1; i++) {
+      let items = prompt("Что приносит дополнительный доход? (Перечислите через запятую все источники)", "");
+      if ((typeof (items)) === 'string' && items != null && items != '') {
+        appData.extraIncome = items.split(', ');
+        appData.extraIncome.push(prompt('Может что-то еще?)', ''));
+        appData.extraIncome.sort();
+        appData.extraIncome.forEach(function (item, x) {
+          console.log('Способы доп. заработка: ' + (x + 1) +'. ' + item);
+        });
+
+      } else {
+        --i;
+      }
+    }
+    
+
+
+ 
+  }
+};
+
+appData.chooseExpenses();
+appData.detectDayBudget();
+appData.detectLevel();
+appData.checkSavings();
+appData.chooseOptExpenses();
+appData.chooseIncome();
+
+for (let key in appData) {
+  console.log('Наша программа включает в себя данные: ' + key + ' имеет значение ' + appData[key]);
+}
